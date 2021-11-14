63633efb2dc0a81ef5bc415503f49bfb232ac45e
Fedor Borshev <fedor@borshev.com>
2021-11-14 18:16:25 +0300

    Production: aws env var typo fix

--- rekognition.py
+++ rekognition.py
@@ -10,7 +10,7 @@ load_dotenv()
 client = boto3.client(
     'rekognition',
     aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
-    aws_secret_access_key=os.getenv('AWS_SECRET_ACCES_KEY'),
+    aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
     region_name='us-east-1',
 )
 

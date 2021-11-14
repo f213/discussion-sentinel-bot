8baad8809a63c8f796bd7530ee33e0048eedb9c9
Fedor Borshev <fedor@borshev.com>
2021-11-14 17:31:02 +0300

    Logging messages and actions to PostgreSQL (#1)

--- bot.py
+++ bot.py
@@ -1,3 +1,5 @@
+from typing import Final, Optional
+
 import logging
 import operator
 import os
@@ -8,16 +10,43 @@ from telegram.ext import CallbackContext, MessageHandler, Updater
 from telegram.ext.filters import Filters, MessageFilter
 from urlextract import URLExtract
 
+DB_ENABLED: Final[bool] = os.getenv('DATABASE_URL') is None
+
+
+def log_message(message: Message, action: Optional[str] = ''):
+    """Create a log entry for telegram message"""
+    from models import LogEntry
+
+    if message is None or not DB_ENABLED:
+        return
+
+    LogEntry.create(
+        user_id=message.from_user.id,
+        chat_id=message.chat_id,
+        message_id=message.message_id,
+        text=message.text,
+        meta=dict(),
+        raw=message.to_dict(),
+        action=action,
+    )
+
 
 def delete(update: Update, context: CallbackContext):
     message = update.message or update.edited_message
     if message is None:
         return
 
+    log_message(message, action='delete')
     message.bot.delete_message(
         message_id=message.message_id,
         chat_id=message.chat_id,
     )
+
+
+def log_message_and_metadata(update: Update, context: CallbackContext):
+    message = update.message or update.edited_message
+
+    log_message(message)
 
 
 class ContainsTelegramContact(MessageFilter):
@@ -70,7 +99,10 @@ def init_sentry():
         sentry_sdk.init(sentry_dsn)
 
 
-def main():
+if __name__ == '__main__':
+    from dotenv import load_dotenv
+    load_dotenv()
+
     bot_token = os.getenv('BOT_TOKEN')
     app_name = os.getenv('HEROKU_APP_NAME')
 
@@ -81,6 +113,13 @@ def main():
     bot.dispatcher.add_handler(
         delete_messages_that_match(ContainsLink()),
     )
+
+    if DB_ENABLED:  # log all not handled messages
+        from models import create_tables
+        create_tables()
+        bot.dispatcher.add_handler(
+            MessageHandler(filters=Filters.text, callback=lambda update, context: log_message(update.message or update.edited_message)),
+        )
 
     if in_heroku():
         init_sentry()
@@ -94,10 +133,3 @@ def main():
     else:  # bot is running on the dev machine
         enable_logging()
         bot.start_polling()
-
-
-if __name__ == '__main__':
-    from dotenv import load_dotenv
-    load_dotenv()
-
-    main()
